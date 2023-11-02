044d2769bc7d48d293ae20b4b660421ff3f472dc
祥冥 孔 <1980873747@qq.com>
2023-11-02 18:01:54 +0800

    重构

---
+++ YunWeiTools/NetworkWatchDog/NetworkWatchDog/Configuration.cs
@@ -0,0 +1,135 @@
+﻿namespace NetworkWatchDog
+{
+    public class Configuration
+    {
+        public BaseSetting baseSetting
+        {
+            get; set;
+        } = new();
+
+        public ErrorReport errorReport
+        {
+            get; set;
+        } = new();
+    }
+
+    public class BaseSetting
+    {
+        public int IntranettripTime
+        {
+            get; set;
+        }
+        public List<IpGroup> NetworkGroup
+        {
+            get; set;
+        }
+        public int ExternaltripTime
+        {
+            get; set;
+        }
+
+        public int TimeOut
+        {
+            get; set;
+        } = 5000;
+
+        public int PingTimer
+        {
+            get; set;
+        } = 1000;
+        public int BuffurMaxLine
+        {
+            get; set;
+        } = 10000;
+    }
+
+    public class IpGroup
+    {
+        #region 基础信息
+        public string Ipconfig
+        {
+            get; set;
+        }
+        public bool isMachine
+        {
+            get; set;
+        }
+        public string IpName
+        {
+            get; set;
+        }
+
+        public string MachineInfo
+        {
+            get; set;
+        }
+
+        public string MachineLocation
+        {
+            get; set;
+        }
+
+        public bool isintra
+        {
+            get; set;
+        } = true;
+        #endregion
+
+        public DateTime ErrorReportTime
+        {
+            get; set;
+        } = DateTime.Now;
+        private List<ErrorInfo> infos
+        {
+            get; set;
+        } = new List<ErrorInfo>();
+
+        public IpGroup AddError(ErrorInfo errorInfo,int Min)
+        {
+            DateTime Mintime = DateTime.Now.AddMinutes(-Min);
+            infos.Add(errorInfo);
+            infos.RemoveAll(x => x.ErrorTime<Mintime);
+
+            return this;
+        }
+
+        public bool TryReport()
+        {
+            if(this.infos.Count>=20)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+
+    public class ErrorReport
+    {
+        public bool isReportError
+        {
+            get; set;
+        }
+
+        public int ReportMinTimes
+        {
+            get; set;
+        }
+        public int SkipTime
+        {
+            get; set;
+        }
+    }
+
+    public class ErrorInfo
+    {
+        public DateTime ErrorTime
+        {
+            get; set;
+        }
+        public string ErrorReportContent
+        {
+            get; set;
+        } = "";
+    }
+}
--- YunWeiTools/NetworkWatchDog/NetworkWatchDog/Form1.cs
+++ YunWeiTools/NetworkWatchDog/NetworkWatchDog/Form1.cs
@@ -1,14 +1,14 @@
 using System.Net.NetworkInformation;
 
-using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace NetworkWatchDog
 {
-    //todo api报警
     public partial class Form1:Form
     {
-        private List<Ipaddress> ipTextBoxes;
-        private List<Ipaddress> ListiningIP;
+        public Configuration configura = new();
+
+        private List<string> ListiningIP;
         int IntranettripTime = 10, ExternaltripTime = 150;
         int PingTimer = 1000, BuffurMaxLine = 10000;
         int timeout = 5000;
@@ -16,105 +16,116 @@ namespace NetworkWatchDog
         public Form1()
         {
             InitializeComponent();
-
-            ipTextBoxes=new List<Ipaddress>();
+            //SendReportToDingDingTEST();
             ListiningIP=new();
+        }
+
+        private async void SendReportToDingDingTEST()
+        {
+            using HttpClient client = new();
+            // 构造POST请求的内容
+            var postData = new FormUrlEncodedContent(new[]
+            {
+                //报警群组设置为系统组
+                new KeyValuePair<string, string>("grounp", "xitongzu"),
+                //设置报警时触发at联系人
+                new KeyValuePair<string, string>("at", "18751936236,17361914131"),
+                //报警信息
+                new KeyValuePair<string, string>("message", "{ipname} ap连接故障报警：\r\n\r\nip：{ip}\r\n型号：{ipinfo}\r\n位置：{iplocation}\r\n信息：此ap一分钟内出现了36次连接故障。请登录向日葵查看详情\r\n")
+            });
+
+            // 发送POST请求
+            HttpResponseMessage response = await client.PostAsync("https://www.kujiang.com/Tool/dingding",postData);
+
+            if(response.IsSuccessStatusCode)
+            {
+                // 获取响应内容
+                string responseContent = await response.Content.ReadAsStringAsync();
+            }
+            else
+            {
+            }
         }
 
         private void pingConnecting()
         {
-            foreach(var ipAddress in ipTextBoxes.Where(ipAddress => !ListiningIP.Contains(ipAddress)))
+            foreach(var ipAddress in configura.baseSetting.NetworkGroup.Where(ip => !ListiningIP.Contains(ip.Ipconfig)))
             {
                 // 启动ping线程
-                ListiningIP.Add(ipAddress);
-                new Thread(() => Ping_PingCompleted(ipAddress)).Start();
+                ListiningIP.Add(ipAddress.Ipconfig);
+                new Thread(() => Ping_PingCompleted(ipAddress.Ipconfig)).Start();
             }
         }
 
         private void MainForm_Load(object sender,EventArgs e)
         {
-            try
+            configura=JsonConvert.DeserializeObject<Configuration>(File.ReadAllText("config.json"));
+            if(configura==null)
             {
-                var configBuilder = new ConfigurationBuilder()
-               .SetBasePath(Directory.GetCurrentDirectory())
-               .AddJsonFile("config.json",optional: true,reloadOnChange: true);
-
-                var config = configBuilder.Build();
-                var IntranetGroup = config.GetSection("IntranetGroup");
-                var IntranetGroupChildren = IntranetGroup.GetChildren();
-
-                var ExternalNetworkGroup = config.GetSection("ExternalNetworkGroup");
-                var ExternalNetworkChildren = ExternalNetworkGroup.GetChildren();
-
-#nullable disable //trycatch 判断是否为空
-                string[] IntranetAddress = IntranetGroupChildren.Select(x => x.Value).ToArray();
-                string[] ExternalNetworkAddresses = ExternalNetworkChildren.Select(x => x.Value).ToArray();
-                IntranettripTime=int.Parse(config.GetSection("IntranettripTime").Value);
-                ExternaltripTime=int.Parse(config.GetSection("ExternaltripTime").Value);
-                timeout=int.Parse(config.GetSection("TimeOut").Value);
-
-
-                PingTimer=int.Parse(config.GetSection("PingTimer").Value);
-                BuffurMaxLine=int.Parse(config.GetSection("BuffurMaxLine").Value);
-#nullable enable
-
-                ipTextBoxes.Clear();
-                ipTextBoxes=(from ip in IntranetAddress select new Ipaddress() { ipinfo=ip.ToString(),isintra=true }).ToList();
-                ipTextBoxes.AddRange((from ip in ExternalNetworkAddresses select new Ipaddress() { ipinfo=ip.ToLower(),isintra=false }).ToList());
-
-
-                listBox1.Items.Clear();
-                foreach(var ip in ipTextBoxes)
-                {
-                    listBox1.Items.Add(ip.ipinfo);
-                }
-
-
-                this.Text=$"网络连接监视器 --内/外网ip延迟 --{IntranettripTime}/{ExternaltripTime}ms";
-
-                pingConnecting();
+                return;
             }
-            catch
+
+            listBox1.Items.Clear();
+
+            foreach(var ip in configura.baseSetting.NetworkGroup)
             {
-                MessageBox.Show(Directory.GetCurrentDirectory()+"config文件缺失");
+                listBox1.Items.Add(ip.Ipconfig+"  "+ip.IpName);
             }
+
+            this.Text=$"网络连接监视器 --内/外网ip延迟 --{configura.baseSetting.IntranettripTime}/{configura.baseSetting.ExternaltripTime}ms";
+
+            pingConnecting();
+
         }
 
-        private void Ping_PingCompleted(object ipadress)
+        private void Ping_PingCompleted(object ip)
         {
             while(true)
             {
-                Ipaddress ip = (Ipaddress)ipadress;
-                Ping ping = new();
+                string ipconfig = (string)ip;
+                IpGroup ipGroup = configura.baseSetting.NetworkGroup.FindLast(x => x.Ipconfig==ipconfig);
+                if(ipGroup!=null)
+                {
+                    Ping ping = new();
 
-                try
-                {
-                    PingReply reply = ping.Send(ip.ipinfo,timeout);
-                    if(reply.Status==IPStatus.Success)
+                    try
                     {
-                        UpdateUI($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} --{ip.ipinfo}: 字节={reply.Buffer.Length} 时间={reply.RoundtripTime}ms TTL={reply.Options?.Ttl}",reply.RoundtripTime,ip.isintra);
+                        PingReply reply = ping.Send(ipGroup.Ipconfig,configura.baseSetting.TimeOut);
+
+
+
+                        if(reply.Status==IPStatus.Success)
+                        {
+                            UpdateUI(ipGroup.Ipconfig,$": 字节={reply.Buffer.Length} 时间={reply.RoundtripTime}ms TTL={reply.Options?.Ttl}",reply.RoundtripTime,ipGroup.isintra);
+                        }
+                        else
+                        {
+                            //不成功汇总
+                            UpdateUI(ipGroup.Ipconfig,$": {reply.Status}",-1);
+                        }
                     }
-                    else
+                    catch(Exception ex)
                     {
-                        //不成功汇总
-                        UpdateUI($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} --{ip.ipinfo}: {reply.Status}",-1);
+                        //异常报告
+                        UpdateUI(ipGroup.Ipconfig,$": {ex.Message}",-1);
                     }
+                    ping.Dispose();
+                    Thread.Sleep(PingTimer); // 等待1秒后再次执行ping命令
                 }
-                catch(Exception ex)
+                else
                 {
-                    //异常报告
-                    UpdateUI($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} --{ip.ipinfo}: {ex.Message}",-1);
+                    UpdateUI("","缓存错误,找不到ip信息",-1);
                 }
-                ping.Dispose();
-                Thread.Sleep(PingTimer); // 等待1秒后再次执行ping命令
             }
         }
 
-        private void UpdateUI(string message,long outtime,bool istra = true)
+        private void UpdateUI(string ip,string info,long outtime,bool istra = true)
         {
+            string message = $"{DateTime.Now:yyyy-MM-mm HH:mm:ss} --{ip}";
+
             if(InvokeRequired)
             {
-                Invoke(new Action<string,long,bool>(UpdateUI),message,outtime,istra);
+                Invoke(new Action<string,string,long,bool>(UpdateUI),ip,info,outtime,istra);
             }
             else
             {
@@ -138,18 +149,61 @@ namespace NetworkWatchDog
                     // 执行长时间运行的操作
                     if(outtime>=triptime||outtime==-1)
                     {
-                        Invoke((Action)(() =>
+                        Invoke((() =>
                         {
-                            richTextBox1.AppendText(message+Environment.NewLine);
-                            File.AppendAllText(Directory.GetCurrentDirectory()+"/error.log",message+"\r\n");
+                            richTextBox1.AppendText(message+info+Environment.NewLine);
+                            File.AppendAllText(Directory.GetCurrentDirectory()+"/error.log",message+info+"\r\n");
                         }));
                     }
 
-                    Invoke((Action)(() =>
+                    Invoke((() =>
                     {
-                        richTextBox2.AppendText(message+Environment.NewLine);
+                        richTextBox2.AppendText(message+info+Environment.NewLine);
                     }));
                 });
+            }
+        }
+
+        private void UpdateUiNew(PingReply reply,IpGroup group)
+        {
+            if(InvokeRequired)
+            {
+                Invoke(new Action<PingReply,IpGroup>(UpdateUiNew),reply,group);
+            }
+            else
+            {
+                if(richTextBox1.Lines.Count()>configura.baseSetting.BuffurMaxLine)
+                {
+                    var a = richTextBox1.Text.Remove(richTextBox1.GetFirstCharIndexFromLine(100));
+
+                    richTextBox1.Text=a;
+                    richTextBox1.Refresh();
+                }
+                if(richTextBox2.Lines.Count()>configura.baseSetting.BuffurMaxLine)
+                {
+                    var a = richTextBox2.Text.Remove(0,richTextBox2.GetFirstCharIndexFromLine(100));
+
+                    richTextBox2.Text=a;
+                    richTextBox2.Refresh();
+                }
+
+                bool issuccess = false;
+                string message = "";
+                if(reply!=null)
+                {
+                    if(reply.Status==IPStatus.Success)
+                    {
+                        issuccess=true;
+                    }
+                    else
+                    {
+
+                    }
+                }
+
+
+
+
             }
         }
 
@@ -167,16 +221,4 @@ namespace NetworkWatchDog
         }
     }
 
-    public class Ipaddress
-    {
-        public string ipinfo
-        {
-            get; set;
-        } = "";
-
-        public bool isintra
-        {
-            get; set;
-        }
-    }
 }
