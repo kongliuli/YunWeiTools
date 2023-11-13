4579d4f83f074fb22b1daf41640bb54ecefefdad
祥冥 孔 <1980873747@qq.com>
2023-11-13 14:25:35 +0800

    13

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using StackExchange.Redis;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Connection();
+
+
+
+        Console.ReadLine();
+    }
+
+    public static void Connection()
+    {
+
+        // 创建Redis客户端
+        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("hssc16.3hvkim.clustercfg.use1.cache.amazonaws.com:6379,abortConnect=false");
+
+        // 获取数据库
+        IDatabase db = redis.GetDatabase(0);
+        Console.WriteLine(db.KeyExists("1"));
+
+        var a = redis.GetDatabase();
+
+
+
+        Console.WriteLine(a);
+
+    }
+}
