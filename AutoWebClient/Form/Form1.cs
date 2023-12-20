cc67a26d276b21bffed311213198a1a64ab00cae
祥冥 孔 <1980873747@qq.com>
2023-12-20 17:05:26 +0800

    111

--- AutoWebClient/Form/Form1.cs
+++ AutoWebClient/Form/Form1.cs
@@ -13,18 +13,22 @@ namespace AWCForm
 
         private void WebbrowserInit()
         {
-            // 将应用程序的名称和版本号添加到注册表项
-            var appName = System.IO.Path.GetFileName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
-            var version = new Version(11,0,0,0);
-            var regKey = Microsoft.Win32.Registry.CurrentUser.CreateSubKey
-                ($@"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION\{appName}");
-            regKey.SetValue(appName,version.Major*1000+version.Minor,Microsoft.Win32.RegistryValueKind.DWord);
 
             // 创建WebBrowser控件并指定使用Microsoft Edge浏览器内核
-            var webBrowser = new WebBrowser();
-            webBrowser.Navigate("https://usercenter2.aliyun.com/ri/summary?spm=5176.9843921.content.12.62374882fJfxy9&commodityCode=&accounttraceid=51c17d5fd52b4c0eb8bcfbd6167e5ea0rygq");
-
+            var webBrowser = new CefSharp.WinForms.ChromiumWebBrowser();
+            //webBrowser.LoadUrl("https://ie.icoa.cn/");
+            webBrowser.LoadUrl("https://account.aliyun.com/login/login.htm?oauth_callback=https%3A%2F%2Fusercenter2.aliyun.com%2Fri%2Fsummary%3Fspm%3D5176.9843921.content.12.62374882fJfxy9%26commodityCode%3D");
             webBrowser.Dock=DockStyle.Fill;
+            webBrowser.LoadingStateChanged+=(sender,args) =>
+            {
+                if(args.IsLoading==false)
+                {
+                    // 页面加载完成
+                }
+            };
+
+
+
             this.Controls.Add(webBrowser);
         }
     }
